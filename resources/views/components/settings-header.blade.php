0e627b4b123c07deefdb3c2f3ef826f42edfc893
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-27 19:34:37 +0200

    Create migration for categories table in database

---
+++ resources/views/components/settings-header.blade.php
@@ -0,0 +1,35 @@
+@php use Illuminate\Support\Facades\Route; @endphp
+
+<div class="heading mt-[7px]">
+    <div class="border-b-[1px] border-[#e4dfdf]">
+        <div class="pl-[30px] pb-[21px]">
+            <h1>
+                Podešavanja
+            </h1>
+        </div>
+    </div>
+</div>
+<div class="py-4 text-gray-500 border-b-[1px] border-[#e4dfdf] pl-[30px]">
+    <a href="{{ route('settings.policies.index') }}"
+       class="inline @if(Route::current()->getName() == 'settings.policies.index') active-book-nav @endif">
+        Polisa
+    </a>
+    <a href="settingsKategorije.php" class="inline ml-[70px] hover:text-blue-800 active-book-nav">
+        Kategorije
+    </a>
+    <a href="settingsZanrovi.php" class="inline ml-[70px] hover:text-blue-800">
+        Zanrovi
+    </a>
+    <a href="settingsIzdavac.php" class="inline ml-[70px] hover:text-blue-800">
+        Izdavac
+    </a>
+    <a href="settingsPovez.php" class="inline ml-[70px] hover:text-blue-800">
+        Povez
+    </a>
+    <a href="settingsFormat.php" class="inline ml-[70px] hover:text-blue-800">
+        Format
+    </a>
+    <a href="settingsPismo.php" class="inline ml-[70px] hover:text-blue-800">
+        Pismo
+    </a>
+</div>
--- resources/views/pages/settings/policy.blade.php
+++ resources/views/pages/settings/policy.blade.php
@@ -7,38 +7,8 @@
 @section('page_content')
     <section class="w-screen h-screen pl-[80px] py-4 text-gray-700">
         <!-- Heading of content -->
-        <div class="heading mt-[7px]">
-            <div class="border-b-[1px] border-[#e4dfdf]">
-                <div class="pl-[30px] pb-[21px]">
-                    <h1>
-                        Settings
-                    </h1>
-                </div>
-            </div>
-        </div>
-        <div class="py-4 text-gray-500 border-b-[1px] border-[#e4dfdf] pl-[30px]">
-            <a href="{{route('settings.policies.index')}}" class="inline hover:text-blue-800 active-book-nav">
-                Polisa
-            </a>
-            <a href="settingsKategorije.php" class="inline ml-[70px] hover:text-blue-800">
-                Kategorije
-            </a>
-            <a href="settingsZanrovi.php" class="inline ml-[70px] hover:text-blue-800">
-                Zanrovi
-            </a>
-            <a href="settingsIzdavac.php" class="inline ml-[70px] hover:text-blue-800">
-                Izdavac
-            </a>
-            <a href="settingsPovez.php" class="inline ml-[70px] hover:text-blue-800">
-                Povez
-            </a>
-            <a href="settingsFormat.php" class="inline ml-[70px] hover:text-blue-800">
-                Format
-            </a>
-            <a href="settingsPismo.php" class="inline ml-[70px] hover:text-blue-800">
-                Pismo
-            </a>
-        </div>
+        <x-settings-header></x-settings-header>
+
         <div class="height-ucenikProfile pb-[30px] scroll">
             <!-- Space for content -->
             <div class="section- mt-[20px]">
