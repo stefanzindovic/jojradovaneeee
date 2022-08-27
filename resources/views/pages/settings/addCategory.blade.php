672e756e61642f8fb364abb9dca02a1efe462961
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-28 01:08:04 +0200

    Create route and controller for create new category screen

--- resources/views/pages/settings/addCategory.blade.php
+++ resources/views/pages/settings/addCategory.blade.php
@@ -19,7 +19,8 @@
                         <nav class="w-full rounded">
                             <ol class="flex list-reset">
                                 <li>
-                                    <a href="settingsPolisa.php" class="text-[#2196f3] hover:text-blue-600">
+                                    <a href="{{ route('settings.policies.index') }}"
+                                       class="text-[#2196f3] hover:text-blue-600">
                                         Settings
                                     </a>
                                 </li>
@@ -27,7 +28,8 @@
                                     <span class="mx-2">/</span>
                                 </li>
                                 <li>
-                                    <a href="settingsKategorije.php" class="text-[#2196f3] hover:text-blue-600">
+                                    <a href="{{ route('settings.categories.index') }}"
+                                       class="text-[#2196f3] hover:text-blue-600">
                                         Kategorije
                                     </a>
                                 </li>
@@ -35,9 +37,9 @@
                                     <span class="mx-2">/</span>
                                 </li>
                                 <li>
-                                    <a href="#" class="text-gray-400 hover:text-blue-600">
+                                    <p class="text-gray-400 disabled">
                                         Nova kategorija
-                                    </a>
+                                    </p>
                                 </li>
                             </ol>
                         </nav>
@@ -52,9 +54,9 @@
                     <div class="w-[50%] mb-[100px]">
                         <div class="mt-[20px]">
                             <p>Naziv kategorije <span class="text-red-500">*</span></p>
-                            <input type="text" name="nazivKategorije" id="nazivKategorije"
+                            <input type="text" name="title" id="categoryTitle"
                                    class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"
-                                   onkeydown="clearErrorsNazivKategorije()" />
+                            />
                             <div id="validateNazivKategorije"></div>
                         </div>
 
@@ -65,8 +67,7 @@
                                 <div class="bg-gray-300 h-[40px] w-[102px] px-[20px] pt-[10px]">
                                     <label class="cursor-pointer">
                                         <p class="leading-normal">Browse...</p>
-                                        <input id="icon-upload" type='file' class="hidden" :multiple="multiple"
-                                               :accept="accept" />
+                                        <input name="icon" id="icon-upload" type='file' class="hidden"/>
                                     </label>
                                 </div>
                                 <div id="icon-output" class="h-[40px] px-[20px] pt-[7px]"></div>
@@ -75,23 +76,20 @@
 
                         <div class="mt-[20px]">
                             <p class="inline-block">Opis</p>
-                            <textarea name="opisKategorije" rows="10"
-                                      class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]">
-
-                                </textarea>
+                            <textarea name="description" rows="10"
+                                      class="flex w-[90%] mt-2 px-2 py-2 text-base bg-white border border-gray-300 shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-[#576cdf]"></textarea>
                         </div>
                     </div>
                 </div>
                 <div class="absolute bottom-0 w-full">
                     <div class="flex flex-row">
                         <div class="inline-block w-full text-white text-right py-[7px] mr-[100px]">
-                            <button type="button"
-                                    class="btn-animation shadow-lg mr-[15px] w-[150px] focus:outline-none text-sm py-2.5 px-5 transition duration-300 ease-in bg-[#F44336] hover:bg-[#F55549] rounded-[5px]">
-                                Ponisti <i class="fas fa-times ml-[4px]"></i>
-                            </button>
-                            <button id="sacuvajKategoriju" type="submit"
-                                    class="btn-animation shadow-lg w-[150px] disabled:opacity-50 focus:outline-none text-sm py-2.5 px-5 transition duration-300 ease-in rounded-[5px] hover:bg-[#46A149] bg-[#4CAF50]"
-                                    onclick="validacijaKategorija()">
+                                <button type="reset"
+                                        class="btn-animation shadow-lg mr-[15px] w-[150px] focus:outline-none text-sm py-2.5 px-5 transition duration-300 ease-in bg-[#F44336] hover:bg-[#F55549] rounded-[5px]">
+                                    Ponisti <i class="fas fa-times ml-[4px]"></i>
+                                </button>
+                            <button id="saveCategory" type="submit"
+                                    class="btn-animation shadow-lg w-[150px] disabled:opacity-50 focus:outline-none text-sm py-2.5 px-5 transition duration-300 ease-in rounded-[5px] hover:bg-[#46A149] bg-[#4CAF50]">
                                 Sacuvaj <i class="fas fa-check ml-[4px]"></i>
                             </button>
                         </div>
