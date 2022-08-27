891ad21a5d53b31120573edc495d4beab709899f
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-27 21:33:45 +0200

    Add datatables cdn into project

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -7,3 +7,6 @@
 <!-- File upload -->
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 <script src="https://unpkg.com/create-file-list"></script>
+
+<!-- Datatables -->
+<script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
--- resources/views/components/styles.blade.php
+++ resources/views/components/styles.blade.php
@@ -1,2 +1,5 @@
 @vite('./resources/css/style.css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+
+<!-- Datatables -->
+<link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
