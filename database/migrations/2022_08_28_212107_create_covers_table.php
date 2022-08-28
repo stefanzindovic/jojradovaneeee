af5878f16510be3c4d0f04cd48b1f83cf3a7311b
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-28 23:29:01 +0200

    Define table migrations for covers

--- database/migrations/2022_08_28_212107_create_covers_table.php
+++ database/migrations/2022_08_28_212107_create_covers_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('covers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
