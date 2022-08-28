333d4e0f0dfe79b91e4b563ae0598107d2da96f9
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-29 01:59:42 +0200

    Get all scripts from database and return them on scripts index page

--- app/Http/Controllers/ScriptController.php
+++ app/Http/Controllers/ScriptController.php
@@ -5,17 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Script;
 use App\Http\Requests\StoreScriptRequest;
 use App\Http\Requests\UpdateScriptRequest;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 
 class ScriptController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Application|Factory|View
      */
-    public function index()
+    public function index(): View|Factory|Application
     {
-        //
+        $scripts = Script::orderBy('id', 'DESC')->get();
+        return view('..pages.settings.index', compact('scripts'));
     }
 
     /**
