a9cddfb2b0ebad1255cdc182de755ca27d1aaff8
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-29 03:54:13 +0200

    Define migration for language table

--- database/migrations/2022_08_29_015209_create_languages_table.php
+++ database/migrations/2022_08_29_015209_create_languages_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('languages', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
