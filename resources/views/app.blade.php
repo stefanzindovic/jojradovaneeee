a91a29113d4b6e96eca535373fca032bfc58ea16
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-30 01:50:47 +0200

    Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Online biblioteka | @yield('page_title')</title>
+
+    <link rel="icon" type="image/x-icon" href="{{asset('imgs/library-favicon.ico')}}">
+
     <x-styles></x-styles>
 </head>
 <body>
