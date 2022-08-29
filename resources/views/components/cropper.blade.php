b1dec20e6284d81995e501a415eea7c195ea6d35
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-29 20:46:55 +0200

    Create cropper.js file and fix ui glich with cropper frame error message

--- resources/views/components/cropper.blade.php
+++ resources/views/components/cropper.blade.php
@@ -1,19 +1,25 @@
 @props(['picture' => 'profile-picture-placeholder.jpg', 'stage' => null])
 
-<label class="mt-6 cursor-pointer">
-    <div style="width: 220px; height: 220px;" id="empty-cover-art" class="relative py-[48px] text-center border-2 border-gray-300 border-solid">
-        <div class="py-4">
-            @if($picture === 'profile-picture-placeholder.jpg')
-                <img id="img-place-holder" class="absolute w-full h-full bottom-0" src="{{asset('imgs/profile-picture-placeholder.jpg')}}">
-            @else
-                <img id="img-place-holder" class="absolute w-full h-full bottom-0" src="{{asset('storage/uploads/' . $stage . '/' . $picture)}}">
-            @endif
-            <input type="file" name="picture" class="hidden" accept="image/png,image/gif,image/jpg,image/jpeg,image/bim,image/webp,image/svg">
+<div class="flex flex-col">
+    <label class="mt-6 cursor-pointer">
+        <div style="width: 220px; height: 220px;" id="empty-cover-art"
+             class="relative py-[48px] text-center border-2 border-gray-300 border-solid">
+            <div class="py-4">
+                @if($picture === 'profile-picture-placeholder.jpg')
+                    <img id="img-place-holder" class="absolute w-full h-full bottom-0"
+                         src="{{asset('imgs/profile-picture-placeholder.jpg')}}">
+                @else
+                    <img id="img-place-holder" class="absolute w-full h-full bottom-0"
+                         src="{{asset('storage/uploads/' . $stage . '/' . $picture)}}">
+                @endif
+                <input type="file" name="picture" class="hidden"
+                       accept="image/png,image/gif,image/jpg,image/jpeg,image/bim,image/webp,image/svg">
+            </div>
+            <img id="image-output-student" class="hidden absolute w-48 h-[188px] bottom-0">
         </div>
-        <img id="image-output-student" class="hidden absolute w-48 h-[188px] bottom-0">
-    </div>
-</label>
-@error("picture")
-<p style="color:red;" id="errorMessageByLaravel"><i class="fa fa-times  mr-[5px] mt-[10px]"></i> {{ $message }}</p>
-@enderror
+    </label>
+    @error("picture")
+    <p style="color:red; max-width: 220px;" id="errorMessageByLaravel"><i class="fa fa-times  mr-[5px] mt-[10px]"></i> {{ $message }}</p>
+    @enderror
+</div>
 
