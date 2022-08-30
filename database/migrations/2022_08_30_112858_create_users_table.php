570a66dd3c53a36545bd404d9def5dcf7a4f560d
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-30 13:36:04 +0200

    Create default admin user in users table migration

--- database/migrations/2022_08_30_112858_create_users_table.php
+++ database/migrations/2022_08_30_112858_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -20,13 +21,25 @@ return new class extends Migration
             $table->string('jmbg', 13)->unique();
             $table->string('username', 18)->unique();
             $table->string('picture')->default('profile-picture-placeholder.jpg');
-            $table->foreign('role_id')->references('id')->on('user_roles');
+            $table->foreignId('role_id')->constrained('user_roles');
+            $table->boolean('is_active')->default(true);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->softDeletes();
             $table->timestamps();
         });
+
+        // Create admin user
+        $model = new \App\Models\User();
+        $model->name = 'Administrator';
+        $model->email = 'administrator@administrator.com';
+        $model->jmbg = '0000000000000';
+        $model->username = 'administrator';
+        $model->role_id = 1;
+        $model->email_verified_at = now();
+        $model->password = \Illuminate\Support\Facades\Hash::make('admin123admin');
+        $model->remember_token = Str::random(10);
+        $model->save();
     }
 
     /**
