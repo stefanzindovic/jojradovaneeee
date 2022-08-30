82035c9295bec5bf589f38e34bfabf098bf6a153
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-30 13:12:38 +0200

    Create default user roles insade migration

--- database/migrations/2022_08_30_110617_create_user_roles_table.php
+++ database/migrations/2022_08_30_110617_create_user_roles_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -18,6 +17,16 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        $roles = [
+            'Administrator', 'Bibliotekar', 'Učenik',
+        ];
+
+        foreach ($roles as $role) {
+            $model = new \App\Models\UserRole();
+            $model->name = $role;
+            $model->save();
+        }
     }
 
     /**
