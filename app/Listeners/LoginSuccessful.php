e4f0034c844cd3e1e4762cb297925e40e7d81484
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-31 19:34:54 +0200

    Disalow students to authenticate themselfs on admin panel

--- app/Listeners/LoginSuccessful.php
+++ app/Listeners/LoginSuccessful.php
@@ -4,8 +4,13 @@ namespace App\Listeners;
 
 use App\Models\UserLogins;
 use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Auth;
-use IlluminateAuthEventsLogin;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -24,11 +29,17 @@ class LoginSuccessful
     /**
      * Handle the event.
      *
-     * @param  Login  $event
-     * @return void
+     * @param Login $event
+     *
      */
     public function handle(Login $event)
     {
+        // if user is student
+        if(Auth::user()->role_id == 3) {
+            Auth::logout();
+            Session::flush();
+            return abort(401);
+        }
         // change last login value
         $model = new UserLogins();
         $model->user_id = Auth::user()->id;
