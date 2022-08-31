068770bedf01609748f54ae1a887d397b6f85756
stefanzindovic <stefanzindovicc@gmail.com>
2022-08-31 18:14:12 +0200

    Remove add new librarian option if authenticated user is not administrator

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -49,12 +49,14 @@
                     <div class="absolute right-[12px] w-56 mt-[35px] origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
                         aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
                         <div class="py-1">
+                            @if(\Illuminate\Support\Facades\Auth::user()->role_id == 1)
                             <a href="{{route('librarians.create')}}" tabindex="0"
                                 class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600"
                                 role="menuitem">
                                 <i class="far fa-address-book mr-[8px] ml-[5px] py-1"></i>
                                 <span class="px-4 py-0">Bibliotekar</span>
                             </a>
+                            @endif
                             <a href="{{ route('students.create') }}" tabindex="0"
                                 class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600"
                                 role="menuitem">
