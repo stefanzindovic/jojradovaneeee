f4772d853764dd7313413cec53c09fe50e8c4bd3
stefanzindovic <stefanzindovicc@gmail.com>
2022-09-05 03:22:25 +0200

    Create file for password inputs validation

--- resources/views/components/styles.blade.php
+++ resources/views/components/styles.blade.php
@@ -1,6 +1,6 @@
 @vite('./resources/css/style.css')
 @vite('./resources/css/cropper.min.css')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <!-- Datatables -->
 <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
