ba66fbf27dbae90d13891cd9b08fb104caf129a8
stefanzindovic <stefanzindovicc@gmail.com>
2022-09-13 00:03:48 +0200

    Define controller finction for rendering issue book index page

--- app/Http/Controllers/IssueBookController.php
+++ app/Http/Controllers/IssueBookController.php
@@ -2,6 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Policy;
+use App\Models\User;
+
 class IssueBookController extends Controller
 {
+    public function index(Book $book)
+    {
+        $policy = Policy::find(1);
+        $students = User::where('role_id', 3)->get();
+
+        return view('..pages.books.actions.issue', compact('policy', 'students', 'book'));
+    }
 }
