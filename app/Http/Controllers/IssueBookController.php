cd7e487845991b0cff120d3cd78db1c626b319ff
stefanzindovic <stefanzindovicc@gmail.com>
2022-09-13 22:53:29 +0200

    Create controller function for issuing book to student

--- app/Http/Controllers/IssueBookController.php
+++ app/Http/Controllers/IssueBookController.php
@@ -3,16 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\BookAction;
+use App\Models\BookActionStatus;
+use App\Models\BooksUnderAction;
 use App\Models\Policy;
 use App\Models\User;
+use Illuminate\Http\Client\Request as ClientRequest;
+use Illuminate\Http\Request as HttpRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
 
 class IssueBookController extends Controller
 {
     public function index(Book $book)
     {
-        $policy = Policy::find(1);
+        $policy = Policy::findOrFail(1);
         $students = User::where('role_id', 3)->get();
 
         return view('..pages.books.actions.issue', compact('policy', 'students', 'book'));
+    }
+
+    public function issue(Book $book, HttpRequest $request)
+    {
+        $input = $request->validate([
+            'student_id' => 'required|numeric',
+            'action_start' => 'required',
+            'action_deadline' => 'required',
+        ]);
+
+        try {
+            // Generate and save new model for book under action
+            $bookUnderActionModel = new BooksUnderAction();
+            $bookUnderActionModel->book()->associate($book);
+            $bookUnderActionModel->student()->associate($input['student_id']);
+            $bookUnderActionModel->save();
+
+            $bookActionModel = new BookAction();
+            $bookActionModel->book()->associate($bookUnderActionModel);
+            $bookActionModel->librarian()->associate(Auth::user());
+            $bookActionModel->status()->associate(1);
+            $bookActionModel->action_start = $input['action_start'];
+            $bookActionModel->action_deadline = $input['action_deadline'];
+            $bookActionModel->save();
+
+            return to_route('books.index')->with('successMessage', 'Knjiga je uspješno izdata.');
+        } catch (\Throwable $th) {
+            dd($th);
+            return back()->with('errorMessage', 'Nešto nije u redu. Molimo vas da polušate ponovo.');
+        }
     }
 }
