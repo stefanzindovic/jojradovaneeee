8f9ab311f763fdada08d3be39e0e46249fcbad94
stefanzindovic <stefanzindovicc@gmail.com>
2022-09-13 17:56:42 +0200

    Define relationship between book action status and book actions

--- app/Models/BookAction.php
+++ app/Models/BookAction.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookAction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public function status()
+    {
+        return $this->belongsTo(BookActionStatus::class, 'action_status_id');
+    }
 }
