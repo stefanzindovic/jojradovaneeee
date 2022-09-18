692750734699716c55debedad06b54fe012c28fb
stefanzindovic <stefanzindovicc@gmail.com>
2022-09-18 20:16:44 +0200

    Render dashboard blade template

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BookAction;
 use Illuminate\Http\Request;
 
 class ActivityController extends Controller
 {
     public function dashboard()
     {
-        return view('..pages.books.activities.dashboard');
+        $activities = BookAction::with(['status', 'book', 'originalBook', 'librarian'])->orderBy('id', 'desc')->get();
+        return view('..pages.books.activities.dashboard', compact('activities'));
     }
 }
